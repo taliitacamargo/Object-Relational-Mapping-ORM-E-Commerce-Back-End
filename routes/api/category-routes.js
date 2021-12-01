d82a1523f9f5fd60c45bdb926816f1caf9668ce8
Tali <taliitacamargo@icloud.com>
2021-12-01 14:12:42 -0500

    new

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,25 +3,74 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/',async (req, res) => {
+  try{
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all categories
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id',async (req, res) => {
+  try{
+  const categoryData = await Category.findByPk(req.params.id, {
+    include: [{model: Product, as: 'product_category'}]
+  });
+
+  if (!categoryData) {
+    res.status(404).json({ message: 'No category found with this id!' });
+    return;
+  }
+  res.status(200).json(categoryData);
+} catch (err) {
+  res.status(500).json(err);
+}
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
+  try{
+  const categoryData = await Category.create(req.body);
+  res.status(200).json(categoryData);
+} catch (err) {
+  res.status(400).json(err);
+}
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id',async (req, res) => {
+  try{
+    const categoryData = await Category.update(req.params.id,
+       {include: [{model:Product, as: 'product_category'}]
+      })
+      if (!categoryData) {
+        res.status(404).json({ message: 'No category found with this id!!!' });
+        return;
+      }
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',async (req, res) => {
+  try{
+    const categoryData = await Trip.destroy({
+      where: {id: req.params.id}
+    });
+    if (!tripData) {
+      res.status(404).json({message: 'No category with this id 4'});
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // delete a category by its `id` value
 });
 
